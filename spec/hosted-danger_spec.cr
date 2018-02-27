2e0dddfc32589562f2e5f62510a7148a9a0a25db
taicsuzu <taicsuzu@yahoo-corp.jp>
2018-02-27 14:24:04 +0900

    fix

--- spec/hosted-danger_spec.cr
+++ spec/hosted-danger_spec.cr
@@ -1,13 +1,4 @@
+ENV["SPEC"] = "true"
+
 require "./spec_helper"
-
-describe HostedDanger do
-  it "successfully build" do
-    proj_root = File.expand_path("../..", __FILE__)
-
-    Dir.cd(proj_root) do
-      system("shards build").should eq(true)
-    end
-  end
-end
-
 require "./units/*"
