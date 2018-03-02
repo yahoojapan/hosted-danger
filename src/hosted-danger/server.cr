d3868cdf00b418896516498a03e088f35a110bf2
taicsuzu <taicsuzu@yahoo-corp.jp>
2018-03-02 13:28:42 +0900

    fix

--- src/hosted-danger/server.cr
+++ src/hosted-danger/server.cr
@@ -9,9 +9,6 @@ module HostedDanger
     def initialize
       @health_check = HealthCheck.new
       @web_hook = WebHook.new
-      # @ghe_proxy = GitProxy.new("ghe.corp.yahoo.co.jp")
-      # @partner_proxy = GitProxy.new("partner.git.corp.yahoo.co.jp")
-      # @git_proxy = GitProxy.new("git.corp.yahoo.co.jp")
       @git_proxy = GitProxy.new
     end
 
@@ -24,15 +21,6 @@ module HostedDanger
       post "/hook" { |context, params| @web_hook.hook(context, params) }
 
       # Internal Proxy
-      # get "/proxy/ghe/*" { |context, params| @ghe_proxy.proxy_get(context, params) }
-      # get "/proxy/partner/*" { |context, params| @partner_proxy.proxy_get(context, params) }
-      # get "/proxy/git/*" { |context, params| @git_proxy.proxy_get(context, params) }
-      # post "/proxy/ghe/*" { |context, params| @ghe_proxy.proxy_post(context, params) }
-      # post "/proxy/partner/*" { |context, params| @partner_proxy.proxy_post(context, params) }
-      # post "/proxy/git/*" { |context, params| @git_proxy.proxy_post(context, params) }
-      # patch "/proxy/ghe/*" { |context, params| @ghe_proxy.proxy_patch(context, params) }
-      # patch "/proxy/partner/*" { |context, params| @partner_proxy.proxy_patch(context, params) }
-      # patch "/proxy/git/*" { |context, params| @git_proxy.proxy_patch(context, params) }
       get "/proxy/:symbol/*" { |context, params| @git_proxy.proxy_get(context, params) }
       post "/proxy/:symbol/*" { |context, params| @git_proxy.proxy_post(context, params) }
       patch "/proxy/:symbol/*" { |context, params| @git_proxy.proxy_patch(context, params) }
