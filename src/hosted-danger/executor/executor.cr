942985071377f5d2f7458f66ffa08d84c9bcdf9f
taicsuzu <taicsuzu@yahoo-corp.jp>
2018-03-06 19:17:38 +0900

    try?

--- src/hosted-danger/executor/executor.cr
+++ src/hosted-danger/executor/executor.cr
@@ -37,8 +37,6 @@ module HostedDanger
       exec_cmd(repo_tag, "git remote add origin #{remote_from_html_url(html_url, access_token)}", dir, env)
       # exec_cmd(repo_tag, "git fetch origin pull/#{pr_number}/head --depth 50", dir, env)
       # exec_cmd(repo_tag, "git reset --hard FETCH_HEAD", dir, env)
-      exec_cmd(repo_tag, "git fetch --depth 50", dir, env)
-      exec_cmd(repo_tag, "git reset --hard #{sha}", dir, env)
 
       config_wrapper = ConfigWrapper.new(dir)
 
