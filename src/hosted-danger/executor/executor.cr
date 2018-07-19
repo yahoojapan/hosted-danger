9c2d2f3137b1ae3b79677e88345a2a8bd4675ff5
taicsuzu <taicsuzu@yahoo-corp.jp>
2018-07-19 17:22:05 +0900

    rm debug logs

--- src/hosted-danger/executor/executor.cr
+++ src/hosted-danger/executor/executor.cr
@@ -33,11 +33,7 @@ module HostedDanger
       exec_cmd("git config --local http.postBuffer 1048576000", dir)
       exec_cmd("git remote add origin #{remote_from_html_url(html_url, access_token)}", dir)
       exec_cmd("timeout #{TIMEOUT_FETCH} git fetch origin #{base_branch} --depth #{behind_by + 1}", dir)
-      exec_cmd("git rev-list --max-parents=0 FETCH_HEAD", dir)
-      exec_cmd("git rev-parse --verify FETCH_HEAD", dir)
       exec_cmd("timeout #{TIMEOUT_FETCH} git fetch origin +refs/pull/#{pr_number}/head --depth #{ahead_by + 1}", dir)
-      exec_cmd("git rev-parse --verify FETCH_HEAD", dir)
-      exec_cmd("git rev-list --max-parents=0 FETCH_HEAD", dir)
       exec_cmd("git reset --hard FETCH_HEAD", dir)
 
       config_wrapper.load
