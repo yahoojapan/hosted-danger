4966a9b0badf26a38950db0c3747f8a724e77ba1
taicsuzu <taicsuzu@yahoo-corp.jp>
2018-11-15 09:51:00 +0900

    fix

--- src/hosted-danger/server/slack_proxy.cr
+++ src/hosted-danger/server/slack_proxy.cr
@@ -4,13 +4,19 @@ module HostedDanger
   class SlackProxy
     def post(context, params)
       if payload = context.request.body.try &.gets_to_end
+        p payload
+
         slack_payload = SlackPayload.from_json(payload)
+
+        p slack_payload
 
         L.info "slack (##{slack_payload[:channel]}): #{slack_payload[:text]}"
 
         headers = HTTP::Headers.new
         headers["Authorization"] = "Bearer #{ServerConfig.secret("slack_bot_token")}"
         headers["Content-Type"] = "application/json"
+
+        p headers
 
         res = HTTP::Client.post("https://slack.com/api/chat.postMessage", headers, slack_payload.to_json)
 
