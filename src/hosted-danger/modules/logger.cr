200d3755fa74f898076a7bf8ea0652cc325b5489
Taichiro Suzuki <taicsuzu@taicsuzu.docker.ssk.ynwm.yahoo.co.jp>
2019-01-17 10:33:30 +0900

    fix

--- src/hosted-danger/modules/logger.cr
+++ src/hosted-danger/modules/logger.cr
@@ -2,23 +2,17 @@ module HostedDanger
   alias L = Logger
 
   class Logger
-    extend Paster
-
-    MYM_TOKEN = "feb6396e272a344c8aa6d43e42c59b7ce9f4e9f466a9b4bc973da97196d22c9c"
-
-    def self.info(msg : String, mym = false) : Nil
+    def self.info(msg : String) : Nil
       log_out("Info".colorize.fore(:green).to_s, msg)
-      post_mym(msg) if mym
       nil
     end
 
-    def self.error(msg : String, mym = true) : Nil
+    def self.error(msg : String) : Nil
       log_out("Error".colorize.fore(:red).to_s, msg)
-      post_mym(msg) if mym
       nil
     end
 
-    def self.error(e : Exception, payload : String?, show_backtrace : Bool = true, mym : Bool = true) : Nil
+    def self.error(e : Exception, payload : String?, show_backtrace : Bool = true) : Nil
       message : String = if error_message = e.message
         error_message
       else
@@ -27,22 +21,15 @@ module HostedDanger
 
       backtrace = (e.backtrace? || [] of String).join("\n") rescue ""
 
-      paster_url = if _payload = payload
-                     upload_text(_payload)
-                   else
-                     "No payload"
-                   end
-
       message = "<< Message >>:\n#{message}\n\n"
       backtrace = show_backtrace ? "<< Backtrace >>\n```\n#{backtrace}\n```\n\n" : ""
-      log = "<< Log >>\n#{paster_url}"
+      log = "<< Log >>\n#{payload}"
 
-      L.error "#{message}#{backtrace}#{log}", mym
+      L.error "#{message}#{backtrace}#{log}"
     end
 
-    def self.warn(msg : String, mym = false) : Nil
+    def self.warn(msg : String) : Nil
       log_out("Warn".colorize.fore(:yellow).to_s, msg)
-      post_mym(msg) if mym
       nil
     end
 
@@ -53,18 +40,6 @@ module HostedDanger
 
     def self.ftime : String
       Time.now.to_s("%Y-%m-%d %H:%M:%S")
-    end
-
-    def self.post_mym(msg : String)
-      headers = HTTP::Headers.new
-      headers["Content-type"] = "application/json"
-
-      body = {
-        token:   MYM_TOKEN,
-        message: msg,
-      }.to_json
-
-      HTTP::Client.post("https://mym.corp.yahoo.co.jp/api/post", headers, body)
     end
   end
 end
