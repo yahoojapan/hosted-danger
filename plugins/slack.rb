4376dc9000e4aaf45ff533a16c9d9b0d450919e3
taicsuzu <taicsuzu@yahoo-corp.jp>
2019-01-25 17:08:39 +0900

    add 'link_names: true' of Slack API

--- plugins/slack.rb
+++ plugins/slack.rb
@@ -11,11 +11,12 @@ module Danger
       http = Net::HTTP.new(uri.host, uri.port)
 
       req = Net::HTTP::Post.new(uri.path)
-      req['Content-Type'] = 'application/json'
+      req['Content-Type'] = 'application/json; charset=UTF-8'
 
       payload = {
         text: text,
-        channel: channel
+        channel: channel,
+        link_names: true,
       }
 
       req.body = payload.to_json
